1c2f2680d6b1e33c2a75ca5cff02ce443d572a3a
AArora10 <77709572+AArora10@users.noreply.github.com>
2021-06-25 15:56:00 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+function preload(){
+
+}
+
+function setup(){
+
+    canvas = createCanvas ( 500 , 500) ;
+    canvas.position ( 110 , 250 ) ; 
+  
+
+    
+
+}
+
+function draw(){
+
+   
+   rectangle_2 =  rect(30, 80, 55, 55); 
+   rectangle_3 =  rect(30, 100, 55, 55); 
+   rectangle_4 =  rect(30, 120, 55, 55); 
+   rectangle_5 =  rect(30, 140, 55, 55); 
+   rectangle_6 =  rect(30, 160, 55, 55); 
+   rectangle_7 =  rect(30, 180, 55, 55);
+   rectangle_8 =  rect(30, 200, 55, 55);
+   rectangle_9 =  rect(30, 220, 55, 55);
+   rectangle_10 =  rect(30, 240, 55, 55);
+   rectangle_11 =  rect(30, 260, 55, 55);
+   rectangle_12 =  rect(30, 280, 55, 55);
+   rectangle_13 =  rect(30, 300, 55, 55);
+   rectangle_14 =  rect(30, 320, 55, 55);
+   rectangle_15 =  rect(30, 340, 55, 55);
+   rectangle_16 =  rect(30, 360, 55, 55);
+   rectangle_17 =  rect(30, 380, 55, 55);
+   
+   rectangle_2 =  rect(80, 80, 55, 55); 
+   rectangle_3 =  rect(100, 80, 55, 55); 
+   rectangle_4 =  rect(120, 80, 55, 55); 
+   rectangle_5 =  rect(140, 80, 55, 55); 
+   rectangle_6 =  rect(160, 80, 55, 55); 
+   rectangle_7 =  rect(180, 80, 55, 55);
+   rectangle_8 =  rect(200, 80, 55, 55);
+   rectangle_9 =  rect(220, 80, 55, 55);
+   rectangle_10 =  rect(240, 80, 55, 55);
+   rectangle_11 =  rect(260, 80, 55, 55);
+   rectangle_12 =  rect(280, 80, 55, 55);
+   rectangle_13 =  rect(300, 80, 55, 55);
+   rectangle_14 =  rect(320, 80, 55, 55);
+   rectangle_15 =  rect(340, 80, 55, 55);
+   rectangle_16 =  rect(360, 80, 55, 55);
+   rectangle_17 =  rect(380, 80, 55, 55);
+
+   rec1 = rect(380,80,55,350);
+   rec2 = rect(30,380,350,55);
+
+    
+  
+}
+
+
+
+
+
+
+
